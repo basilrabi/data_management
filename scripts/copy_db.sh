60e0d2dfd0275c69b0c0a6a10f91348fb526eba2
basilrabi <ericbasil.rabi@gmail.com>
2019-09-04 22:43:57 +0800

    disable creation of extension since origin db has extension already

--- scripts/copy_db.sh
+++ scripts/copy_db.sh
@@ -4,7 +4,6 @@ db_cmd='-h localhost -U djangotest'
 
 psql $db_cmd postgres -c "drop database if exists data_management_test"
 psql $db_cmd postgres -c "create database data_management_test"
-psql $db_cmd data_management_test -c "create extension postgis"
 
 pg_dump -h pi -U djangotest -c --if-exists -O data_management | \
 psql $db_cmd -d data_management_test
