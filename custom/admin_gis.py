4ea4b9ab3bc2046ba431647db4939d6d1ef9e0bf
basilrabi <ericbasil.rabi@gmail.com>
2019-11-17 15:38:16 +0800

    change wms layer to topo for geomodel admin

--- custom/admin_gis.py
+++ custom/admin_gis.py
@@ -5,7 +5,7 @@ class TMCLocationAdmin(admin.GeoModelAdmin):
     default_lat = 9.5235
     default_zoom = 14
     wms_name = 'TMC'
-    wms_url = 'http://192.169.101.192:8010/ogc/map'
-    wms_layer = 'Taganito Mine General Location'
+    wms_url = 'http://192.169.101.192:8010/ogc/topo'
+    wms_layer = 'Topography'
     num_zoom = 25
     openlayers_url = 'http://192.169.101.192:81/static/OpenLayers-2.13.1/OpenLayers.js'
