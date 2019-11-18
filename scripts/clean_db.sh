63f8d99f90b70228a8412a10c94c9bbe0cef37d5
basilrabi <ericbasil.rabi@gmail.com>
2019-11-18 14:27:40 +0800

    use python configuration in db credentials

--- scripts/clean_db.sh
+++ scripts/clean_db.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-db_cmd='-h localhost -U djangotest'
+set -a
+. data_management/local.py
+set +a
 
-psql $db_cmd postgres -c "drop database if exists data_management_test"
-psql $db_cmd postgres -c "create database data_management_test"
-psql $db_cmd data_management_test -c "create extension postgis"
+psql -h $db_host -U $db_user -w postgres -c "drop database if exists $db_name"
+psql -h $db_host -U $db_user -w postgres -c "create database $db_name"
+psql -h $db_host -U $db_user -w $db_name -c "create extension postgis"
