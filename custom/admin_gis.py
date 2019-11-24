4af5d9717ef875c89996b0c3de9307bfcd25ed41
basilrabi <ericbasil.rabi@gmail.com>
2019-11-24 15:46:20 +0800

    use domain name of server

--- custom/admin_gis.py
+++ custom/admin_gis.py
@@ -5,9 +5,9 @@ class TMCLocationAdmin(admin.GeoModelAdmin):
     default_lat = 9.5235
     default_zoom = 14
     wms_name = 'TMC'
-    wms_url = 'http://192.169.101.192:8010/ogc/topo'
+    wms_url = 'http://datamanagement.tmc.nickelasia.com:8010/ogc/topo'
     wms_layer = 'Topography'
     num_zoom = 25
-    openlayers_url = 'http://192.169.101.192:81/static/OpenLayers-2.13.1/OpenLayers.js'
+    openlayers_url = 'http://datamanagement.tmc.nickelasia.com:81/static/OpenLayers-2.13.1/OpenLayers.js'
     map_width = 800
     map_height = 600
