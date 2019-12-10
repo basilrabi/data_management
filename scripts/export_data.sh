ada87eb0825db68db80889ec28920312ab624283
basilrabi <ericbasil.rabi@gmail.com>
2019-12-10 09:52:04 +0800

    use csv view in data exportation

--- scripts/export_data.sh
+++ scripts/export_data.sh
@@ -6,24 +6,15 @@ then
 fi
 mkdir data
 
-mv data_management/local.py data_management/local.py.bak
-cat >data_management/local.py <<EOL
-db_name = '$DATA_MANAGEMENT_DB_NAME'
-db_user = '$DATA_MANAGEMENT_DB_USER'
-db_password = '$DATA_MANAGEMENT_DB_PASSWORD'
-db_host = '$DATA_MANAGEMENT_DB_HOST'
-db_port = '$DATA_MANAGEMENT_DB_PORT'
-EOL
-
 ogr2ogr -f "GPKG" data/location_mineblock.gpkg \
     "PG:host=$DATA_MANAGEMENT_DB_HOST user=$DATA_MANAGEMENT_DB_USER dbname=$DATA_MANAGEMENT_DB_NAME" \
     location_mineblock
 
-./manage.py shell < scripts/export_data/shipment_lct.py
-./manage.py shell < scripts/export_data/shipment_lctcontract.py
-./manage.py shell < scripts/export_data/shipment_vessel.py
-./manage.py shell < scripts/export_data/shipment_shipment.py
-./manage.py shell < scripts/export_data/shipment_trip.py
-./manage.py shell < scripts/export_data/shipment_tripdetail.py
-
-mv data_management/local.py.bak data_management/local.py
+curl datamanagement.tmc.nickelasia.com/shipment/export/lct -o data/shipment_lct.csv
+curl datamanagement.tmc.nickelasia.com/shipment/export/lctcontract -o data/shipment_lctcontract.csv
+curl datamanagement.tmc.nickelasia.com/shipment/export/vessel -o data/shipment_vessel.csv
+curl datamanagement.tmc.nickelasia.com/shipment/export/shipment -o data/shipment_shipment.csv
+curl datamanagement.tmc.nickelasia.com/shipment/export/laydaysstatement -o data/shipment_laydaysstatement.csv
+curl datamanagement.tmc.nickelasia.com/shipment/export/laydaysdetail -o data/shipment_laydaysdetail.csv
+curl datamanagement.tmc.nickelasia.com/shipment/export/trip -o data/shipment_trip.csv
+curl datamanagement.tmc.nickelasia.com/shipment/export/tripdetail -o data/shipment_tripdetail.csv
