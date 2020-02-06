c01157b89155e7b8b731e30c49b73f764520879d
basilrabi <ericbasil.rabi@gmail.com>
2020-02-06 15:01:58 +0800

    export groups and permissions

--- custom/urls.py
+++ custom/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('export/users', views.export_users, name='export/users')
+    path('export/group-permissions',
+         views.export_permission_group,
+         name='export-group-permissions'),
+    path('export/groups', views.export_groups, name='export-groups'),
+    path('export/user-permissions',
+         views.export_permission_user,
+         name='export-user-permissions'),
+    path('export/users', views.export_users, name='export-users')
 ]
--- custom/views.py
+++ custom/views.py
@@ -1,6 +1,26 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User
 
 from .functions import export_csv
+
+def export_groups(request):
+    rows = ([
+        str(group.name)
+    ] for group in Group.objects.all())
+    return export_csv(rows, 'groups')
+
+def export_permission_group(request):
+    rows = ([
+        str(group.name),
+        str(permission.codename)
+    ] for group in Group.objects.all() for permission in group.permissions.all())
+    return export_csv(rows, 'group_permission')
+
+def export_permission_user(request):
+    rows = ([
+        str(user.get_username()),
+        str(permission.codename)
+    ] for user in User.objects.all() for permission in user.user_permissions.all())
+    return export_csv(rows, 'user_permission')
 
 def export_users(request):
     rows = ([
