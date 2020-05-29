6c77455b8677c972a9dde142b76f81cb1cf6a46b
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2020-05-29 23:29:50 +0800

    use database without topography schema for rapid testing

--- scripts/clean_db.sh
+++ scripts/clean_db.sh
@@ -4,6 +4,15 @@ set -a
 . data_management/local.py
 set +a
 
+template=data_management_template
+
+if [ $1 == light ]
+then
+    echo "Using light database."
+    template=data_management_template_no_topo
+
+fi
+
 psql -h $db_host -p $db_port -U $db_user -w postgres -c "drop database if exists $db_name"
 psql -h $db_host -p $db_port -U tmcgis -w postgres -c "drop role if exists geology"
 psql -h $db_host -p $db_port -U tmcgis -w postgres -c "drop role if exists gradecontrol"
@@ -13,7 +22,7 @@ psql -h $db_host -p $db_port -U tmcgis -w postgres -c "drop role if exists surve
 # is empty, has the extension postgis_sfcgal enabled and is owned by $db_user.
 # An sql dump of the template is shown in
 # scripts/sql/data_management_template.pgsql
-psql -h $db_host -p $db_port -U $db_user -w postgres -c "create database $db_name template data_management_template"
+psql -h $db_host -p $db_port -U $db_user -w postgres -c "create database $db_name template $template"
 
 # Set-up django database
 ./manage.py migrate
