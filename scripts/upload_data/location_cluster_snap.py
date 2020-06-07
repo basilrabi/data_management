74edcf5b635808d14b470a4b7dffa10e2f5a3944
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2020-06-07 16:50:52 +0800

    arrange snapping of clusters by count, mine_block, ore_class
    fixes #43

--- scripts/upload_data/location_cluster_snap.py
+++ scripts/upload_data/location_cluster_snap.py
@@ -1,16 +1,29 @@
-from location.models.source import Cluster
+# pylint: disable=anomalous-backslash-in-string
+# pylint: disable=import-error
 # pylint: disable=no-member
-for cluster in Cluster.objects.all():
-    if cluster.geom:
-        old_name = cluster.name
-        block = cluster.block_set.first()
-        block.cluster = None
-        block.save()
-        block.cluster = cluster
-        block.save()
-        cluster.refresh_from_db()
-        new_name = cluster.name
-        if old_name == new_name:
-            print(f'{old_name} snapped.', flush=True)
-        else:
-            print(f'{old_name} -> {new_name}.', flush=True)
+from location.models.source import Cluster
+for cluster in Cluster.objects.raw(
+    '''
+    SELECT
+        id,
+        name,
+        substring(name, '^[A-Z](\d+)-\d+-\d+$')::int count,
+        substring(name, '^[A-Z]\d+-\d+-(\d+)$') mine_block,
+        substring(name, '^([A-Z])\d+-\d+-\d+$') ore_class
+    FROM location_cluster
+    WHERE geom IS NOT NULL
+    ORDER BY count, mine_block, ore_class;
+    '''
+):
+    old_name = cluster.name
+    block = cluster.block_set.first()
+    block.cluster = None
+    block.save()
+    block.cluster = cluster
+    block.save()
+    cluster.refresh_from_db()
+    new_name = cluster.name
+    if old_name == new_name:
+        print(f'{old_name} snapped.', flush=True)
+    else:
+        print(f'{old_name} -> {new_name}.', flush=True)
