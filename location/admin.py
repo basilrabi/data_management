af885b92be96fa61d1a337ac1757199ea7a43bf6
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2020-06-19 15:17:55 +0800

    add option to export cluster in list admin

--- location/admin.py
+++ location/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.db import models
 from django.forms import Textarea
+from import_export.admin import ExportMixin
+from import_export.fields import Field
+from import_export.resources import ModelResource
 
 from custom.admin_gis import TMCLocationAdmin
 from sampling.models.proxy import DrillCore
 from .models.landuse import RoadArea
 from .models.source import Cluster, DrillHole, MineBlock, Stockyard
+
+class ClusterResource(ModelResource):
+    cluster_count = Field()
+
+    class Meta:
+        model = Cluster
+        exclude = ('count', 'distance_from_road', 'id', 'geom', 'road')
+
+    def dehydrate_cluster_count(self, cluster):
+        return cluster.block_set.count()
 
 class DrillCoreInline(admin.TabularInline):
     model = DrillCore
@@ -27,9 +40,9 @@ class DrillCoreInline(admin.TabularInline):
     readonly_fields = ['ni', 'fe', 'co']
 
 @admin.register(Cluster)
-class ClusterAdmin(TMCLocationAdmin):
-    modifiable = False
-    exclude = ('count',)
+class ClusterAdmin(ExportMixin, admin.ModelAdmin):
+    date_hierarchy = 'date_scheduled'
+    exclude = ('count', 'geom')
     list_display = ('name',
                     'blocks',
                     'mine_block',
@@ -58,6 +71,7 @@ class ClusterAdmin(TMCLocationAdmin):
                        'ore_class',
                        'date_scheduled',
                        'layout_date']
+    resource_class = ClusterResource
     search_fields = ['name', 'mine_block', 'z', 'ore_class']
 
 @admin.register(DrillHole)
