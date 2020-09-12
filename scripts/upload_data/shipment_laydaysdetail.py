70f03a0889a9ef32db3edfd1329f7ec581fe7791
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2020-09-12 16:30:13 +0800

    flush text buffer

--- scripts/upload_data/shipment_laydaysdetail.py
+++ scripts/upload_data/shipment_laydaysdetail.py
@@ -8,8 +8,12 @@ for statement in LayDaysStatement.objects.all():
     try:
         print(f'Computing statement for {statement.__str__()}...')
         statement._compute()
+        sys.stdout.flush()
+        sys.stderr.flush()
     except KeyboardInterrupt:
         print('\nComputing statement interrupted.')
+        sys.stdout.flush()
+        sys.stderr.flush()
         sys.exit(1)
     print('Done.')
     sys.stdout.flush()
