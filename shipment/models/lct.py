43a6b3f268ccefb4ac20d70d22a818263d3a0291
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2021-02-01 14:26:36 +0800

    add sundrying to option

--- shipment/models/lct.py
+++ shipment/models/lct.py
@@ -184,6 +184,7 @@ class TripDetail(models.Model):
         ('waiting_for_dock', 'Waiting for Available Dock'),
         ('waiting_for_tide', 'Waiting for Safe Water Level'),
         ('waiting_for_vessel', 'Waiting for Vessel'),
+        ('sun_drying', 'Sun Drying'),
         ('swell', 'Swell'),
         ('rain', 'Rain'),
         ('rain_swell', 'Rain and Swell'),
