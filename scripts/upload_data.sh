78b18b89021b918a8a2078f2679ea27be1e6f24f
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2021-02-22 17:42:13 +0800

    do not hard code db_user in scripts

--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -102,11 +102,11 @@ sql_script "upload_data" "sampling_drillcoresample" && \
 sql_script "lock" "location_cluster" && \
 sql_script "lock" "location_clusterlayout" && \
 echo "Setting up QGIS users'..." 2>&1 | tee -a log_upload_data && \
-psql -h $db_host -p $db_port -U tmcgis -w postgres -c "create user geology      with inherit encrypted password '$DATA_MANAGEMENT_GEOLOGY'"      2>&1 | tee -a log_upload_data && \
-psql -h $db_host -p $db_port -U tmcgis -w postgres -c "create user gradecontrol with inherit encrypted password '$DATA_MANAGEMENT_GRADECONTROL'" 2>&1 | tee -a log_upload_data && \
-psql -h $db_host -p $db_port -U tmcgis -w postgres -c "create user planning     with inherit encrypted password '$DATA_MANAGEMENT_PLANNING'"     2>&1 | tee -a log_upload_data && \
-psql -h $db_host -p $db_port -U tmcgis -w postgres -c "create user reader       with inherit encrypted password '$DATA_MANAGEMENT_READER'"       2>&1 | tee -a log_upload_data && \
-psql -h $db_host -p $db_port -U tmcgis -w postgres -c "create user survey       with inherit encrypted password '$DATA_MANAGEMENT_SURVEY'"       2>&1 | tee -a log_upload_data && \
+psql -h $db_host -p $db_port -U $db_user -w postgres -c "create user geology      with inherit encrypted password '$DATA_MANAGEMENT_GEOLOGY'"      2>&1 | tee -a log_upload_data && \
+psql -h $db_host -p $db_port -U $db_user -w postgres -c "create user gradecontrol with inherit encrypted password '$DATA_MANAGEMENT_GRADECONTROL'" 2>&1 | tee -a log_upload_data && \
+psql -h $db_host -p $db_port -U $db_user -w postgres -c "create user planning     with inherit encrypted password '$DATA_MANAGEMENT_PLANNING'"     2>&1 | tee -a log_upload_data && \
+psql -h $db_host -p $db_port -U $db_user -w postgres -c "create user reader       with inherit encrypted password '$DATA_MANAGEMENT_READER'"       2>&1 | tee -a log_upload_data && \
+psql -h $db_host -p $db_port -U $db_user -w postgres -c "create user survey       with inherit encrypted password '$DATA_MANAGEMENT_SURVEY'"       2>&1 | tee -a log_upload_data && \
 sql_script "permission" "all"
 sql_script "helper" "excavate_inventory_block"
 sql_script "helper" "excavate_sampling_drillcoresample"
