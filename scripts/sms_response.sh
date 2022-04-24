67df939db0234e01c9352cde6df5af66b281888a
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2022-04-24 14:48:43 +0800

    avoid activating the virtual environment

--- scripts/sms_response.sh
+++ scripts/sms_response.sh
@@ -4,5 +4,4 @@ set -a
 . $HOME/.gunicorn_env
 set +a
 
-source $HOME/.virtualenvs/data_management/bin/activate &&
-$HOME/data_management/manage.py sms_response
+$HOME/.virtualenvs/data_management/bin/python $HOME/data_management/manage.py sms_response
