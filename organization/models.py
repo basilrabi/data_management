0b7aad69e201f84f0c703e94abd6079cc95d6f08
amphibolite <john.velonta@tmc.nickelasia.com>
2023-02-13 15:49:33 +0800

    Added service attribute to Organization

--- organization/models.py
+++ organization/models.py
@@ -1,5 +1,12 @@
 from custom.models import Classification
+from django.db.models import CharField
 
 
 class Organization(Classification):
-    pass
+    service_choice = (
+        ('Contractor', 'Contractor'),
+        ('Operating Company', 'OpCo'),
+        ('Government Agency', 'Govt')
+    )
+
+    service = CharField(max_length = 30, choices=service_choice, null=True, blank= False)
