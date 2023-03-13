fe05b86b22dc15bf30efb827b37cf7dd91104892
amphibolite <john.velonta@tmc.nickelasia.com>
2023-03-13 15:57:54 +0800

    adjustments for date_disposal and date_phased_out
    added date_disposal attribute in the custom model FixedAsset, visible only to superuser and camp admin group.

--- fleet/admin.py
+++ fleet/admin.py
@@ -11,31 +11,56 @@ from .models.equipment import (
 
 @register(Equipment)
 class EquipmentAdmin(ModelAdmin):
-    autocomplete_fields = ['model']
-    exclude = ('equipment_class',)
-    fields = ('fleet_number',
-              'model',
-              'owner',
-              'acquisition_cost',
-              'acquisition_cost_from_accounting',
-              'date_acquired',
-              'date_phased_out',
-              'asset_tag_id',
-              'asset_serial_number',
-              'asset_code',
-              'service_life',
-              'engine_serial_number',
-              'plate_number',
-              'chassis_serial_number',
-              'description',
-              'active')
 
+    autocomplete_fields = ['model']
+    exclude = ('equipment_class',)  
     list_display = ('__str__', 'model', 'plate_number', 'engine_serial_number')
     list_filter = ['owner', 'equipment_class']
     search_fields = ['fleet_number',
                      'model__manufacturer__name',
                      'model__name',
                      'engine_serial_number',]
+
+    fields_1 = ('fleet_number',
+                'model',
+                'owner',
+                'acquisition_cost',
+                'acquisition_cost_from_accounting',
+                'date_acquired')
+
+    fields_2 = ('asset_tag_id',
+                'asset_serial_number',
+                'asset_code',
+                'service_life',
+                'engine_serial_number',
+                'plate_number',
+                'chassis_serial_number',
+                'description',
+                'active')
+
+    def add_view(self, request, form_url='', extra_context=None):
+        
+        if request.user.groups.all().filter(name='camp admin').exists():
+            if_camp = 'date_disposal'
+        elif request.user.is_superuser:
+            if_camp = ('date_phased_out', 'date_disposal')
+        else:
+            if_camp = 'date_phased_out'
+
+        self.fields = self.fields_1 + (if_camp,) + self.fields_2             
+        return super().add_view(request, form_url, extra_context=extra_context)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        
+        if request.user.groups.all().filter(name='camp admin').exists():
+            if_camp = 'date_disposal'
+        elif request.user.is_superuser:
+            if_camp = ('date_phased_out', 'date_disposal')
+        else:
+            if_camp = 'date_phased_out'
+
+        self.fields = self.fields_1 + (if_camp,) + self.fields_2    
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
 
 
 @register(EquipmentClass)
