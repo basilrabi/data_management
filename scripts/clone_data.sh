2bc045bd2dcb8173addd860f70b1df7b970e7012
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2025-01-24 00:28:48 +0800

    update dependencies

--- scripts/clone_data.sh
+++ scripts/clone_data.sh
@@ -10,7 +10,7 @@ if [ -z "${datadir}" ]; then
 fi
 
 lftp -c mirror http://datamanagement.tmc.nickelasia.com:81/applications/data_management_export $datadir
-lftp -c mirror http://datamanagement.tmc.nickelasia.com:81/uploads $DATA_MANAGEMENT_MEDIA_ROOT
+lftp -c mirror http://datamanagement.tmc.nickelasia.com:81/uploads/ ${DATA_MANAGEMENT_MEDIA_ROOT%/}
 ./scripts/clean_db.sh
 ./scripts/upload_data.sh
 
