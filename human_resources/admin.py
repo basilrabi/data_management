edeb4a86f1649477110b53198e9d09e9fe514069
amphibolite <amphibolite@32-002304-CU145.ad-nickelasia.com>
2026-01-03 09:25:00 +0800

    added update_existing policy field

--- human_resources/admin.py
+++ human_resources/admin.py
@@ -3,6 +3,9 @@ from django.contrib.admin import ModelAdmin, register
 from .models import OutgoingCommunication, ExternalIncomingCommunication, Letter, PoliciesAndGuideline
 from organization.models import Division
 
+from django.urls import reverse
+from django.utils.html import format_html
+
 
 @register(OutgoingCommunication)
 class OutgoingCommunicationAdmin(ModelAdmin):
@@ -67,21 +70,45 @@ class LetterAdmin(ModelAdmin):
 @register(PoliciesAndGuideline)
 class PoliciesAndGuidelineAdmin(ModelAdmin):
     date_hierarchy = 'date'
-    fields = ('date',
-              'nature_of_content',
-              'concerned_personnel',
-              'date_of_effectivity',
-              'date_of_expiry',
-              'signed_copy',
-              'cancel',)
+    
+    search_fields = ['transmittal_number', 'nature_of_content']
 
-    list_display = ('date',
-                    'transmittal_number',
-                    'nature_of_content',
-                    'concerned_personnel',
-                    'date_of_effectivity',
-                    'date_of_expiry',
-                    'signed_copy'
+    fields = (
+        'date',
+        'nature_of_content',
+        'concerned_personnel',
+        'update_existing',
+        'date_of_effectivity',
+        'date_of_expiry',
+        'signed_copy',
+        'cancel',
+    )
+
+    autocomplete_fields = ['update_existing']
+
+    list_display = (
+        'date',
+        'transmittal_number',
+        'nature_of_content',
+        'concerned_personnel',
+        'date_of_effectivity',
+        'date_of_expiry',
+        'signed_copy',
+        'get_update_existing_link' 
     )
 
     ordering = ('-date', '-transmittal_number')
+
+    def get_update_existing_link(self, obj):
+        if obj.update_existing:
+            app_label = obj._meta.app_label
+            model_name = obj._meta.model_name
+            view_name = f"admin:{app_label}_{model_name}_change"
+            
+            link_url = reverse(view_name, args=[obj.update_existing.pk])
+            
+            return format_html('<a href="{}">{}</a>', link_url, obj.update_existing)
+        
+        return "-"
+
+    get_update_existing_link.short_description = 'Update Existing'
--- human_resources/models.py
+++ human_resources/models.py
@@ -9,6 +9,7 @@ from django.db.models import (
     Max,
     Model,
     PROTECT,
+    SET_NULL,
     TextField
 )
 from django.utils.html import mark_safe
@@ -156,34 +157,35 @@ class Letter(Model):
     
 
 class PoliciesAndGuideline(Model):
-    """
-    Digital log of HR Policies and Guidelines.
-    """
-
     transmittal_number = CharField(max_length=20, null=False, blank=True, primary_key=True)
     date = DateField(null=True, blank=False)
     nature_of_content = TextField(max_length=99, null=True, blank=False)
     concerned_personnel = CharField(max_length=99, null=True, blank=True)
     date_of_effectivity = DateField(null=True, blank=False)
     date_of_expiry = DateField(null=True, blank=True)
-    signed_copy = FileField(upload_to=filepath_policies,null=True, blank=True)
+    signed_copy = FileField(upload_to='policies/', null=True, blank=True)
+    
+    update_existing = ForeignKey(
+        'self', 
+        on_delete=SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name='updated_by',
+        verbose_name="Update Existing",
+        help_text="Select the existing policy that this entry updates."
+    )
+    
     cancel = BooleanField(null=False, default=False)
 
     def content(self):
         if self.cancel is True:
-            return mark_safe(
-                f'<s>{format(self.nature_of_content)}</s>'
-            )
-        else:
-            return self.nature_of_content
+            return mark_safe(f'<s>{format(self.nature_of_content)}</s>')
+        return self.nature_of_content
 
     def recipient_render(self):
         if self.cancel is True:
-            return mark_safe(
-                f'<s>{format(self.recipient)}</s>'
-            )  
-        else:
-            return self.recipient
+            return mark_safe(f'<s>{format(self.concerned_personnel)}</s>')
+        return self.concerned_personnel
 
     def save(self, *args, **kwargs):
         if not self.transmittal_number:
@@ -192,17 +194,24 @@ class PoliciesAndGuideline(Model):
             else:
                 year = self.date.year
 
-            max_number = PoliciesAndGuideline.objects.filter(transmittal_number__endswith=str(year)).aggregate(Max('transmittal_number'))
+            max_number = PoliciesAndGuideline.objects.filter(
+                transmittal_number__endswith=str(year)
+            ).aggregate(Max('transmittal_number'))
 
             if max_number['transmittal_number__max']:
-                max_number = max_number['transmittal_number__max']
+                max_str = max_number['transmittal_number__max']
             else:
-                max_number = f"HRD-POL-0-{year}"
+                max_str = f"HRD-POL-0-{year}"
 
-            number = int(re.search(r"\-(\d+)\-", max_number).group(1)) + 1
+            match = re.search(r"\-(\d+)\-", max_str)
+            if match:
+                number = int(match.group(1)) + 1
+            else:
+                number = 1
+                
             self.transmittal_number = f"HRD-POL-{number:03d}-{year}"
 
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.transmittal_number
+        return f"{self.transmittal_number} - {self.nature_of_content}"
