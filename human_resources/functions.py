48b240eb6037162b469a5cdef26223b287056928
amphibolite <jkvelonta@gmail.com>
2026-01-24 07:40:39 +0800

    Changed outgoing communications upload file extension from pdf to docx

--- human_resources/functions.py
+++ human_resources/functions.py
@@ -12,7 +12,7 @@ def filepath(instance, filename):
 
 def filepath_original(instance, filename):
     year = re.search(r"\d{4}", instance.transmittal_number).group(0)
-    return f"human_resources/OutgoingCommunications/original/{year}/{instance.transmittal_number}.pdf"
+    return f"human_resources/OutgoingCommunications/original/{year}/{instance.transmittal_number}.docx"
 
 def filepath_letter_original(instance, filename):
     year = re.search(r"\d{4}", instance.transmittal_number).group(0)
